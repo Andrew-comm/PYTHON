63f61bd1e827a5e6d60294a01da784d6a2e886b7
Andrew Rono <ndrwrono2001@gmail.com>
2022-10-30 17:14:06 +0300

    adding graphs to pdfs

--- 100 days of code/pdf.py
+++ 100 days of code/pdf.py
@@ -3,6 +3,12 @@ from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib import colors
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie
+from reportlab.lib.units import inch
+
+report_pie = Pie(width=3*inch, height=3*inch)
+
 report = SimpleDocTemplate("/tmp/report.pdf")
 styles = getSampleStyleSheet()
 report_title = Paragraph("A Complete Inventory of My Fruit", styles["h1"])
@@ -28,8 +34,29 @@ report_table = Table(data=table_data)
 
 table_style = [('GRID', (0,0), (-1,-1), 1, colors.black)]
 report_table = Table(data=table_data, style=table_style, hAlign="LEFT")
-report.build([report_title, report_table])
 
+
+
+
+
+
+report_pie.data = []
+report_pie.labels = []
+for fruit_name in sorted(fruit):
+    report_pie.data.append(fruit[fruit_name])
+    report_pie.labels.append(fruit_name)
+
+    
+
+print(report_pie.data)
+print(report_pie.labels)
+
+report_chart = Drawing()
+report_chart.add(report_pie)
+
+
+
+report.build([report_title, report_table,report_chart])
 
 
 
