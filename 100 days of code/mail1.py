b367703ea03001089a3f5b64ac02c6c502e49788
Andrew Rono <ndrwrono2001@gmail>
2022-11-08 00:30:00 +0300

    added new files

--- 100 days of code/mail1.py
+++ 100 days of code/mail1.py
@@ -1,18 +1,74 @@
-
 #!/usr/bin/env python3
+import mimetypes
 import mailbox
 import smtplib
 import getpass
+import os.path
+import email.message 
 
-mail_server = smtplib.SMTP_SSL('localhost')
+def generate(sender, recipient, subject, body, attachment_path):
 
-#inorder to see messages being sent back and forth 
-mail_server.set_debuglevel(1)
+    ##"""creates an email with attachment"""
+
+    #basic email formatting
+    sender = "localhost"
+    recipient = "you@example.com"
+    message = email.message.EmailMessage()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    message.set_content(body)
+
+
+    #process attachment and add it to email
+
+    attachment_filename = os.path.basename(attachment_path)
+    mime_type, _= mimetypes.guess_type(attachment_path)
+    mime_type, mime_subtype = mime_type.split('/',1)
+
+
+    with open(attachment_path, 'rb') as ap:
+        message.add_attachment(ap.read(),
+                        
+                            maintype = mime_type,
+                            subtype = mime_subtype,
+                            filename = attachment_filename)
+
+        return message
 
 #to authenticate
-
 mail_pass = getpass.getpass('password1')
 print(mail_pass)
 
-mail_server.login(mail_pass)
-(235, b'2.7.0 Accepted')
+
+
+
+
+
+def send(message):
+    mail_server = smtplib.SMTP_SSL('localhost')
+
+    mail_server.login(sender, mail_pass)
+
+#inorder to see messages being sent back and forth 
+    mail_server.set_debuglevel(1)
+    mail_server.send_message(message)
+    mail_server.quit()
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+ 
+
+# mail_server.login(mail_pass)
+# (235, b'2.7.0 Accepted')
