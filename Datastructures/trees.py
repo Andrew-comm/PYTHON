ac3d27d411b8ea55853dfa5703eb571b63f2b940
Andrew <ndrwrono2001@gmail.com>
2022-12-08 14:29:24 +0300

    tree

---
+++ Datastructures/trees.py
@@ -0,0 +1,48 @@
+class TreeNode:
+    def __init__(self,data):
+        self.data = data
+        self.children = []
+        self.parent = None
+
+    def addChild(self,child):
+        child.parent = self
+        self.children.append(child)
+
+def build_product_tree():
+    root = TreeNode("Electronics")
+
+    laptop = TreeNode("laptop")
+    laptop.addChild(TreeNode("Hp"))
+    laptop.addChild(TreeNode("Dell"))
+    laptop.addChild(TreeNode("Mac"))
+
+
+    cellphone = TreeNode("cellphone")
+    cellphone.addChild(TreeNode("Tecno"))
+    cellphone.addChild(TreeNode("Vivo"))
+    cellphone.addChild(TreeNode("Samsung"))
+
+
+    Tv = TreeNode("Tv")
+    Tv.addChild(TreeNode("Dstv"))
+    Tv.addChild(TreeNode("Gotv"))
+    Tv.addChild(TreeNode("Smarttv"))
+
+    root.addChild(laptop)
+    root.addChild(cellphone)
+    root.addChild(Tv)
+
+    return root
+
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    build_product_tree()
+    pass
+
+
